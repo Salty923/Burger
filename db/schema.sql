6e3ca49438d9dcfc349f3596d13dbf14b8a6ff04
Scott Saltzman <salty923@gmail.com>
2018-03-11 23:57:57 -0500

    fix

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,9 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers(
-    ID INT(11) AUTO_INCREMENT NOT NULL,
+    ID INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(250) NOT NULL,
     devoured BOOLEAN DEFAULT false,
+    createdAt TIMESTAMP NOT_NULL,
     PRIMARY KEY(id)
 );
